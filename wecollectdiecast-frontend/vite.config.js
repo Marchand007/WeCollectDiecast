9d4ebd0bc5877995562e6a23e6fb230663ef48f4
Marchand007 <104533250+Marchand007@users.noreply.github.com>
2024-01-25 17:49:48 -0500

    Update vite.config.js

--- wecollectdiecast-frontend/vite.config.js
+++ wecollectdiecast-frontend/vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
